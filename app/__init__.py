b83c9a68dd43e53e7e4fe1734eb99b1bd69b8624
Ravenclaw-OIer <yzshangs@126.com>
2020-09-25 09:44:20 +0800

    Complete structure of __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,19 @@ from flask import flask
 
 def create_app(override_config = None):
   app = Flask(__name__, instance_relative_config = True)
-  app.config_from_mapping(
+  app.config.from_mapping(
     SECRET_KEY = 'secret-key' # NOTE: Don't use this in production!
-    
+    DATABASE = os.path.join(app.instance_path, 'app.sqlite')
   )
+
+  if override_config is not None:
+    app.config.from_mapping(override_config)
+  else:
+    app.config.from_pyfile('config.py', silent = True)
+  
+  try:
+    os.makedirs(app.instance_path)
+  except OSError:
+    pass
+
+  return app
