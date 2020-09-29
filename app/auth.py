62abcef0d60143123a357d610e51554b2a4b0afe
Ravenclaw-OIer <yzshangs@126.com>
2020-09-29 19:45:51 +0800

    Remove password safety validation test

--- app/auth.py
+++ app/auth.py
@@ -6,29 +6,6 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from app.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
-def check_password_strength(pwd):
-  # Comment the next line in production
-  return "OK"
-  # Comment the prev line in production
-  has_letter = False
-  has_number = False
-  for c in pwd:
-    if 'a' <= c and c <= 'z':
-      has_letter = True
-    elif 'A'  <= c and c <= 'Z':
-      has_letter = True
-    elif '0' <= c <= '9':
-      has_number = True
-  
-  if len(pwd) < 6:
-    return '密码长度至少应该为 6''
-  elif not has_letter:
-    return '密码中必须包含至少一个字母'
-  elif not has_number:
-    return '密码中必须包含至少一个数字''
-  else:
-    return 'OK'
-
 @bp.route('/register', methods = ('GET', 'POST'))
 def register():
   if request.method == 'POST':
@@ -41,8 +18,6 @@ def register():
       error = '用户名不能为空'
     elif not password:
       error = '密码不能为空'
-    elif check_password_strength(password) != 'OK':
-      error = check_password_strength(password)
     elif db.execute(
       'SELECT id FROM user WHERE username = ?', 
       (username, )
